0d7f31f47e34bab09b92797afc01acbba7889677
Tsuneo Hakoyama <tunehako@gmail.com>
2024-09-01 23:38:59 +0900

    [add] ref#4 index, login, register pages

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -20,7 +20,6 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
@@ -28,11 +27,10 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
-            'password' => $this->passwordRules(),
+            'password' => 'required',
         ])->validate();
 
         return User::create([
-            'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('page-title')
+会員登録
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('logo')
+<a href="{{ route('home') }}"><img src="{{ asset('storage/image/logo.svg') }}" alt=""></a>
+@endsection
+
+@section('content')
+<main class="main-board">
+    <div class="title">
+        <p>会員登録</p>
+    </div>
+    <form action="/register" method="post" class="input-form">
+        @csrf
+        <div class="input-email">
+            メールアドレス<br>
+            <input type="email" name="email" value="{{ old('email') }}">
+        </div>
+        <div class="error-message">
+            @if ($errors->has('email'))
+            {{ $errors->first('email')}}
+            @endif
+        </div>
+        <div class="input-password">
+            パスワード<br>
+            <input type="password" name="password">
+        </div>
+        <div class="error-message">
+            @if ($errors->has('password'))
+            {{ $errors->first('password')}}
+            @endif
+        </div>
+        <div class="submit">
+            <button type="submit">登録する</button>
+        </div>
+        <div class="page-link">
+            <a href="{{ route('login') }}">ログインはこちら</a>
+        </div>
+    </form>
+</main>
+@endsection
